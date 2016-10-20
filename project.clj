25ded281ecea242a8d503f80df9d827d793b1b62
Brian Mingus <brian@thinktopic.com>
2016-10-20 13:26:51 -0600

    Version 0.0.1

--- project.clj
+++ project.clj
@@ -7,4 +7,15 @@
                  [prismatic/schema "1.0.1"]
                  [clj-time "0.11.0"]
                  [com.google.api-client/google-api-client "1.20.0"]
-                 [com.google.apis/google-api-services-analytics "v3-rev116-1.20.0"]])
+                 [com.google.apis/google-api-services-analytics "v3-rev116-1.20.0"]]
+  :repositories {"snapshots" {:url "s3p://thinktopic.jars/snapshots/"
+                              :passphrase :env
+                              :username :env
+                              :releases false
+                              :sign-releases false}
+                 "releases"  {:url "s3p://thinktopic.jars/releases/"
+                              :passphrase :env
+                              :username :env
+                              :snapshots false
+                              :sign-releases false}}
+  )
